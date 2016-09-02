56a0b1b03c685e49bc180338ce1669a88745e810
rich.d.rich <richard.parratt@sharesight.co.nz>
2016-09-02 15:55:36 +1200

    WIP: iterate

--- lib/flappi/definition_locator.rb
+++ lib/flappi/definition_locator.rb
@@ -4,7 +4,7 @@ module Flappi
 
     def self.locate_class(endpoint_name)
       Flappi.configuration.definition_paths.each do |path|
-        candidate_class = (path.camelize + '::' + endpoint_name).constantize
+        candidate_class = (path.camelize + '::' + endpoint_name).constantize rescue nil
         return candidate_class if candidate_class && candidate_class.included_modules.include?(Flappi::Definition)
       end
 
