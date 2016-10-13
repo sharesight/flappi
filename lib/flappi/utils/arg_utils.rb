8b7dc407ea956cb05cfebd6188b439abc8289f6e
rich.d.rich <richard.parratt@sharesight.co.nz>
2016-10-14 10:31:13 +1300

    Unit test arg_utils

--- lib/flappi/utils/arg_utils.rb
+++ lib/flappi/utils/arg_utils.rb
@@ -3,30 +3,25 @@ module Flappi
     module ArgUtils
 
       # @example Given some args of a form such as:
-      #   a: 1
-      #    a:1, b:2
-      #    :a, 1, :b, 2, :c, 3
-      #    {a: 1, b: 2}, {a: 3}
+      #   a: 1 (hash options)
+      #   :a, 1 (splat)
+      #    a: 1, b: 2 (hash options)
+      #    :a, 1, :b, 2 (splat)
+      #    :a, 1, :b, 2, :c, 3 (splat)
+      #    :a, 1, :b, 2, :a, 3 (splat with repeat)
+      #    {a: 1, b: 2}, {a: 3} (two hashes)
+      #
       #    return an array of name/value pairs (as arrays)
       #    this effectively gives us a multihash where keys can be duplicated
       def self.paired_args(*a)
-        r = if a.is_a? Hash
-          # a hash with unduplicated keys
-          a.to_a
-        elsif a.is_a? Array
-          # an array of either key, value or hashes
-          a.map do |it|
-            if it.is_a? Hash
-              it.to_a
-            else
-              it
-            end
-          end.flatten
-        else
-          raise "Expecting paired arguments of some kind at #{a}"
-        end
-
-        r.each_slice(2).to_a
+        # an array of either key, value or hashes
+        a.map do |it|
+          if it.is_a? Hash
+            it.to_a
+          else
+            it
+          end
+        end.flatten.each_slice(2).to_a
       end
 
     end
---
+++ test/arg_utils_test.rb
@@ -0,0 +1,28 @@
+require_relative 'test_helper'
+
+class ::Flappi::ArgUtilsTest < MiniTest::Test
+
+  context 'paired_args' do
+
+    should 'extract a single pair' do
+      assert_equal [[:a, 1]], Flappi::Utils::ArgUtils::paired_args(:a, 1)
+    end
+
+    should 'extract a single pair from a hash' do
+      assert_equal [[:a, 1]], Flappi::Utils::ArgUtils::paired_args(a: 1)
+    end
+
+    should 'extract two pairs from a hash' do
+      assert_equal [[:a, 1], [:b, 2]], Flappi::Utils::ArgUtils::paired_args(a: 1, b: 2)
+    end
+
+    should 'extract three pairs with repeat' do
+      assert_equal [[:a, 1], [:b, 2], [:a, 3]], Flappi::Utils::ArgUtils::paired_args(:a, 1, :b, 2, :a, 3)
+    end
+
+    should 'extract three pairs from hashes' do
+      assert_equal [[:a, 1], [:b, 2], [:a, 3]], Flappi::Utils::ArgUtils::paired_args({ a: 1, b: 2 }, a: 3)
+    end
+
+  end
+end
