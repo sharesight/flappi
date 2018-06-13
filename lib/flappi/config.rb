8fed15af12bb26ac4fec163009769c19385bdbc6
Michael Martins Kuhinica <taverneiro@gmail.com>
2018-06-13 15:52:22 +1200

    Remove auto array from definition_paths setter

--- lib/flappi/config.rb
+++ lib/flappi/config.rb
@@ -3,16 +3,12 @@
 module Flappi
   class Config
 
-    attr_reader :definition_paths
+    attr_accessor :definition_paths
     attr_accessor :version_plan
     attr_accessor :doc_target_path
     attr_accessor :doc_target_paths
     attr_accessor :logger_level
     attr_reader :logger_target
-
-    def definition_paths=(v)
-      @definition_paths = v.is_a?(Array) ? v : [v]
-    end
 
     # rubocop:disable Naming/AccessorMethodName
     def set_logger_target(&block)
@@ -23,7 +19,6 @@ module Flappi
     def set_logger_rails
       set_logger_target {|m, l| Rails.logger.send %i[error warn info debug debug][l], m }
     end
-
 
   end
 end
