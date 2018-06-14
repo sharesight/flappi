83373b9bb401bb9b5f1e3f71c436513b669e4f3a
Michael Martins Kuhinica <taverneiro@gmail.com>
2018-06-14 16:58:14 +1200

    Fix definition_locator_test

--- test/definition_locator_test.rb
+++ test/definition_locator_test.rb
@@ -6,24 +6,27 @@ class ::Flappi::DefinitionLocatorTest < MiniTest::Test
   context 'locate_class' do
     should 'Locate a valid definition' do
       Flappi.configure do |conf|
-        conf.definition_paths = ['examples']
+        conf.definition_paths = {
+          'V2.0' => 'examples',
+        }
       end
-      located_class = Flappi::DefinitionLocator.locate_class('Exercise1')
+      located_class = Flappi::DefinitionLocator.locate_class('Exercise1', 'V2.0')
       assert_equal Examples::Exercise1, located_class
     end
 
     should 'Raise an error when no valid definition' do
       Flappi.configure do |conf|
-        conf.definition_paths = ['examples']
+        conf.definition_paths = {
+          'V2.0' => 'examples',
+        }
       end
 
       located_class = nil
       ex = assert_raises RuntimeError do
-        located_class = Flappi::DefinitionLocator.locate_class('DoesntExist')
+        located_class = Flappi::DefinitionLocator.locate_class('DoesntExist', 'V2.0')
       end
 
-      assert_equal 'Endpoint DoesntExist is not defined to Flappi: Could not load Examples::DoesntExist because uninitialized constant Examples::DoesntExist was raised',
-                   ex.message
+      assert_equal 'Endpoint DoesntExist is not defined to Flappi: Could not load Examples::DoesntExist because uninitialized constant Examples::DoesntExist was raised', ex.message
       refute located_class
     end
   end
