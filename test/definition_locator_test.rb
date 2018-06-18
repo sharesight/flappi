33e62fb8742138f72ff07c505ca56ac9b98ec605
Kylor Hall <kylor@sharesight.com>
2018-06-18 14:47:59 +1200

    Rubocop hates me.

--- test/definition_locator_test.rb
+++ test/definition_locator_test.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
-require_relative 'test_helper'
 
+require_relative 'test_helper'
 require_relative './examples2/exercise1'
 require_relative './examples2/exercise5'
 
@@ -12,14 +12,14 @@ class ::Flappi::DefinitionLocatorTest < MiniTest::Test
     end
 
     should 'Locate a valid definition by order in the definition_paths config' do
-      # eg. definition_paths = { 'v3.0': ['examples', 'examples2'] } – this test targets the first `examples` and ignores the second.
+      # eg. definition_paths = { 'v3.0': ['examples', 'examples2'] } - this test targets the first `examples` and ignores the second.
       located_class = Flappi::DefinitionLocator.locate_class('Exercise1', 'v3.0')
       assert_equal Examples::Exercise1, located_class
       assert_equal 'examples', located_class.definition_source
     end
 
     should 'Locate a valid version as a fallback by order in the definition_paths config' do
-      # eg. definition_paths = { 'v3.0': ['examples', 'examples2'] } – this test targets the `examples2` as it does not exist in `examples`.
+      # eg. definition_paths = { 'v3.0': ['examples', 'examples2'] } - this test targets the `examples2` as it does not exist in `examples`.
       located_class = Flappi::DefinitionLocator.locate_class('Exercise5', 'v3.0')
       assert_equal Examples2::Exercise5, located_class
       assert_equal 'examples2', located_class.definition_source
