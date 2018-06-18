3690d58584366c7193839077045e720cd318cba1
Kylor Hall <kylor@sharesight.com>
2018-06-18 13:17:45 +1200

    This isn't used; in `test_helper`.

--- test/definition_locator_test.rb
+++ test/definition_locator_test.rb
@@ -5,24 +5,11 @@ require_relative 'test_helper'
 class ::Flappi::DefinitionLocatorTest < MiniTest::Test
   context 'locate_class' do
     should 'Locate a valid definition' do
-      Flappi.configure do |conf|
-        conf.definition_paths = {
-          'v2.0' => 'examples',
-          'v3.0' => 'examples'
-        }
-      end
       located_class = Flappi::DefinitionLocator.locate_class('Exercise1', 'v2.0')
       assert_equal Examples::Exercise1, located_class
     end
 
     should 'Raise an error with a missing version' do
-      Flappi.configure do |conf|
-        conf.definition_paths = {
-          'v2.0' => 'examples',
-          'v3.0' => 'examples'
-        }
-      end
-
       located_class = nil
       ex = assert_raises RuntimeError do
         located_class = Flappi::DefinitionLocator.locate_class('Exercise1', 'v-bad-version')
@@ -33,13 +20,6 @@ class ::Flappi::DefinitionLocatorTest < MiniTest::Test
     end
 
     should 'Raise an error when no valid definition' do
-      Flappi.configure do |conf|
-        conf.definition_paths = {
-          'v2.0' => 'examples',
-          'v3.0' => 'examples'
-        }
-      end
-
       located_class = nil
       ex = assert_raises RuntimeError do
         located_class = Flappi::DefinitionLocator.locate_class('DoesntExist', 'v2.0')
